c3f9b6f31ffdae71082ede523b67a9dc14acaa19
Thomas <edison@ubilinux.(none)>
2000-01-01 00:35:59 +0000

    basic pwm script added

--- misc/EdisonServoTest.py
+++ misc/EdisonServoTest.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+
+
+#conclusion that 3 is rudder, 5 is main sail, and 6 is jib for mraa pin numbers 
+
 import mraa
 import time
 
@@ -10,14 +14,22 @@ def main():
   val = 0;  
     
 
+
   #Servo Constants
-  period = 20000;
-  duty_min = 1100/period;
-  duty_max = 1900/period;
-  
+  period = 20000
+  duty_min = 1100.0 /period;
+  duty_max = 1900.0 /period;
 
   #MRAA setup
-  PWM_PIN = 5;    #TODO: Change this
+
+  #switch to autonomous mode.
+  AUTONOMOUS_SELECT_PIN = 2
+  auto_select_pin = mraa.Gpio(AUTONOMOUS_SELECT_PIN)
+  auto_select_pin.dir(mraa.DIR_OUT)
+  auto_select_pin.write(1)
+
+
+  PWM_PIN = 6;    #pwm pins are the same as listed on the breakout board. (3,5,6)
   pwm = mraa.Pwm(PWM_PIN);
   pwm.period_us(period);   #Period in microseconds, corresponds to 50Hz like the winch motors.
   pwm.enable(True);
@@ -30,10 +42,10 @@ def main():
       print("Duty is out of range");
       continue;
     duty = val;
-    pwm.write((duty_min + (duty_max-duty_min)*duty)/period);       #Scale it according to our ranges
+    writeval = (duty_min + (duty_max-duty_min)*duty)
+    print writeval
+    pwm.write(writeval);       #Scale it according to our ranges
 
  
-
-
 if __name__ == "__main__":
     main()
