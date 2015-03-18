2044245d92a2042847f0a287cd3ac32963d2b3b0
Andrew Malta <andrew.malta@yale.edu>
2015-03-18 13:32:29 -0400

    added gps distance calculation in misc

---
+++ misc/gpsDistance.py
@@ -0,0 +1,28 @@
+from math import radians, cos, sin, asin, sqrt
+
+#returns the distance in km between two gps coordinates.
+def gpsDistance(lat1, lon1, lat2, lon2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+
+while True:
+  lat1 = float(input("lat1 : "))
+  lon1 = float(input("lon1 : "))
+  lat2 = float(input("lat2 : "))
+  lon2 = float(input("lon2 : "))
+
+  print(gpsDistance(lat1, lon1, lat2, lon2))
