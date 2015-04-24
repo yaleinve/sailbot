5111d9b456808ca31ad438fcb0035a4ca0825ca4
Inve Dev Machine <info@yaleigvc.com>
2015-04-24 17:15:43 -0400

    Add randomness to fields in airmar publisher debugging mode

--- src/airmar/src/airmar_node.py
+++ src/airmar/src/airmar_node.py
@@ -10,17 +10,17 @@ class Airmar:
     def __init__(self):
         self.heading = 0
         self.amrRoll = 0
-        self.apWndDir = 0
-        self.apWndSpd = 0
+        self.truWndDir = 0
+        self.truWndSpd = 0
         self.cog = 0
         self.lat = 0
         self.long = 0
         self.sog = 0
 
-        # For debugging, sets a random phase offset so that truWndDir starts
+        # For debugging, sets a random phase offset so that apWndDir starts
         # in a different place from the heading.
-        self.truWndDir = random.random()*360.0
-        self.truWndSpd = 0
+        self.apWndDir = random.random()*360.0
+        self.apWndSpd = 5 # Also for debugging
 
         # Default publishing rate is 10Hz
         self.pub_rate = rospy.Rate(rospy.get_param('rate', 10))
@@ -44,18 +44,22 @@ class Airmar:
             elif self.amrRoll < -45:
                 self.amrRoll = -45
 
-            self.apWndDir = self.apWndDir
-            self.apWndSpd = self.apWndSpd
+            # adds += 0.5 degrees to the current truWndDir
+            self.truWndDir = self.truWndDir + (0.5 - random.random())
+            self.truWndDir = self.truWndDir % 360 # degrees
+
+            # adds += 0.5 units to the current truWndSpd TODO: Knots?
+            self.truWndSpd = self.truWndSpd + (0.5 - random.random())
+            if self.truWndSpd > 10:
+                self.truWndSpd = 10
+            elif self.truWndSpd < 0:
+                self.truWndSpd = 0
+
             self.cog = self.cog
             self.lat = self.lat
             self.long = self.long
             self.sog = self.sog
 
-            # adds += 0.5 degrees to the current truWndDir
-            self.truWndDir = self.truWndDir + (0.5 - random.random())
-            self.truWndDir = self.truWndDir % 360 # degrees
-
-            self.truWndSpd = self.truWndSpd
 
 
     def airmar_pub(self, pub):
@@ -82,8 +86,8 @@ if __name__ == '__main__':
         rospy.init_node('airmar');
         am = Airmar()
 
-        pub = rospy.Publisher('airmar', AirmarData, queue_size = 10)
-        rostopic.loginfo("[airmar] Started airmar node!")
+        pub = rospy.Publisher('airmar_data', AirmarData, queue_size = 10)
+        rospy.loginfo("[airmar] Started airmar node!")
 
         while not rospy.is_shutdown():
             # TODO: Implement actual publisher instead of debugging one
