84de1a08419d38b5d834711912a3f8c6c8bdccfe
Eric Anderson <eric.t.anderson@yale.edu>
2015-12-24 08:17:31 -0500

    Airmar node is cleaned for edison but untested

--- src/airmar/src/airmar_runner.py
+++ src/airmar/src/airmar_runner.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+#Updated by Eric Anderson 12/15 to adapt for Edison and recent arch changes,
+#namely that we rely solely on airmar (and camera) for sensing - no navsat
+
 import rospy
 import random
 import math
@@ -7,12 +10,15 @@ import serial
 import pynmea2
 import pdb
 import time
+import mraa
 
-from airmar.msg import AirmarData   #NOTE THAT THIS NO LONGER CONTAINS TRUE WIND FIELDS!!
-from sensor_msgs.msg import NavSatFix
+from airmar.msg import AirmarData
 
 class Airmar:
     def __init__(self):
+        #Set the publishing rate of the airmar to 2Hz for now
+        self.pubInterval = 0.5      # Seonds between publishes
+
         self.heading = 0
         self.amrRoll = 0
         self.truWndDir = 0
@@ -25,26 +31,24 @@ class Airmar:
         self.apWndDir = 0
         self.apWndSpd = 0
 
-        # we need to change this port to whatever it's going to be for the
-        # actual machine we run ROS on 
-        self.ser = serial.Serial('/dev/ttyUSB0', 4800, timeout=1)
+        #Initialize a publisher
+        self.pub = rospy.Publisher('/airmar_data', AirmarData, queue_size = 10)
+        
+        #Use mraa to initialize edison pins 0 and 1 to serial
+        mraa.Uart(0)
+        #...but, after initialized, let's use the pyserial library
+        self.ser = serial.Serial('/dev/ttyMFD1', 4800, timeout=1)
         self.ser.readline()
-
-
-        # Default publishing rate is 10Hz
-        self.pub_rate = rospy.Rate(rospy.get_param('rate', 2))
 
     def update_data(self, debug=False):
         '''
         Fetches the latest data off the serial wire from the Airmar
-        debug -- if True, publish fake data, else, publish real data
+        debug -- if True, publish fake data, else, publish real data (Not yet implemented)
         '''
         line = self.ser.readline()
         if "," in str(line):
             try:
                 msg = pynmea2.parse(line)
-                # self.lat = msg.latitude
-                # self.long = msg.longitude
                 if msg.sentence_type == 'MWV':
                     self.apWndSpd = msg.wind_speed if msg.wind_speed != None else self.apWndSpd
                     self.wndUnits = msg.wind_speed_units if msg.wind_speed_units != None else self.wndUnits
@@ -72,11 +76,10 @@ class Airmar:
 
 
 
-    def airmar_pub(self, pub, pub2):
+    def airmar_pub(self, pub):
         '''
         A publisher that outputs airmar data at a rate of 10Hz
         '''
-        # NOTE: HEADING HAS NOT BEEN UPDATED YET IN update_data
         airmar_data_msg = AirmarData()
         airmar_data_msg.heading = self.heading
         airmar_data_msg.amrRoll = self.amrRoll
@@ -86,40 +89,25 @@ class Airmar:
         airmar_data_msg.sog = self.sog
         airmar_data_msg.truWndSpd = self.truWndSpd
         airmar_data_msg.truWndDir = self.truWndDir
-
-        # The following data is not currently in the AirmarData message spec
-        # As such, it is not being published, but it is still being collected
-        '''airmar_data_msg.cog = self.cog
         airmar_data_msg.lat = self.lat
-        airmar_data_msg.long = self.long
-        airmar_data_msg.sog = self.sog
-        airmar_data_msg.truWndDir = self.truWndDir
-        airmar_data_msg.truWndSpd = self.truWndSpd'''
-
-        lat_long_msg = NavSatFix()
-        lat_long_msg.latitude = self.lat
-        lat_long_msg.longitude = self.long
+        airmar_data_msg.long = self.long      #FIXME: does 'long' overwrite a python keyword?
 
         pub.publish(airmar_data_msg)
-        pub2.publish(lat_long_msg)
 
 if __name__ == '__main__':
     try:
+        #Init the ros node
         rospy.init_node('airmar');
         am = Airmar()
-
-        pub = rospy.Publisher('/airmar_data', AirmarData, queue_size = 10)
-        lat_long_pub = rospy.Publisher('fix', NavSatFix, queue_size = 10)
         rospy.loginfo("[airmar] Started airmar node!")
+        
         tm = rospy.get_time()
 
         while not rospy.is_shutdown():
-            # TODO: Implement actual publisher instead of debugging one
             am.update_data() # Get the latest data from the airmar
-            # add the debug = True flag to enter debug mode
-            if (rospy.get_time() - tm > 0.5):
+            if (rospy.get_time() - tm > am.pubInterval):
                 am.airmar_pub(pub, lat_long_pub) # Publish the latest data
                 tm = rospy.get_time()
-            #am.pub_rate.sleep()
+
     except Exception as e:
         print e
