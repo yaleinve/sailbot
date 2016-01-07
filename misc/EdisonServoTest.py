b2aacee93f77f9d9f7beafc706554bc222795791
Thomas <edison@ubilinux.(none)>
2016-01-07 02:15:53 +0000

    servo testing complete

--- misc/EdisonServoTest.py
+++ misc/EdisonServoTest.py
@@ -23,13 +23,13 @@ def main():
   #MRAA setup
 
   #switch to autonomous mode.
-  AUTONOMOUS_SELECT_PIN = 2   #2 is for rudder, 4 is main/jib
+  AUTONOMOUS_SELECT_PIN = 4   #2 is for rudder, 4 is main/jib
   auto_select_pin = mraa.Gpio(AUTONOMOUS_SELECT_PIN)
   auto_select_pin.dir(mraa.DIR_OUT)   #Really important that you set the input/output of the pin...
   auto_select_pin.write(1)
+  autoval  = 0
 
-
-  PWM_PIN = 6;    #pwm pins are the same as listed on the breakout board. (3,5,6)
+  PWM_PIN = 5;    #pwm pins are the same as listed on the breakout board. (3,5,6)
   pwm = mraa.Pwm(PWM_PIN);
   pwm.period_us(int(period));   #Period in microseconds, corresponds to 50Hz like the winch motors.
   pwm.enable(True);
@@ -41,6 +41,13 @@ def main():
     if val < 0 or val > 1:
       print("Duty is out of range");
       continue;
+    #temporary, swap relay
+    auto_select_pin.write(int(autoval))
+    print('autoval : ' + str(autoval))
+    if autoval == 0:
+      autoval = 1
+    else:
+      autoval = 0
     duty = val;
     writeval = (duty_min + (duty_max-duty_min)*duty)
     print writeval
