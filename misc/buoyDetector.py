cfa27ace9b1e9a2933dba1e9daeade035342231f
Andrew Malta <andrew.malta@yale.edu>
2016-01-20 09:28:52 -0500

    cleaned up code. Bearing todo still

--- misc/buoyDetector.py
+++ misc/buoyDetector.py
@@ -12,87 +12,102 @@ CIRCULARITY_EPSILON_HEIGHT = .35 # constant for determining a cutoff of circular
 MIN_AREA = 1000.0 # the minimum area in pixels that a buoy can be.
 
 #determine if the contour passed in is circular (enough) to be a buoy.
-def is_potential_buoy(cont):
-    area = cv2.contourArea(cont)
-    equi_diameter = np.sqrt(4*area/np.pi)
-    x,y,w,h = cv2.boundingRect(cont)
+#returns true if this contour is potentially a buoy.
+#returns false otherwise.
 
-    print "width: " + str(abs(equi_diameter - w) / w)
+class Contour():
+    def __init__(self, cont):
+        self.area = cv2.contourArea(cont)
+        self.contour = cont
+        self.x, self.y, self.w, self.h = cv2.boundingRect(cont)
+    
+    #returns true if the contour is a potential buoy. false otherwise.
+    def is_potential_buoy(self):
+        equi_diameter = np.sqrt(4*self.area/np.pi)
 
-    if ((abs(equi_diameter - w) / w) < CIRCULARITY_EPSILON_WIDTH and
-        (abs(equi_diameter - h) / h) < CIRCULARITY_EPSILON_HEIGHT and
-        area >= MIN_AREA):
-        print area
-        return True
-    return False
+        if ((abs(equi_diameter - self.w) / self.w) < CIRCULARITY_EPSILON_WIDTH and
+            (abs(equi_diameter - self.h) / self.h) < CIRCULARITY_EPSILON_HEIGHT and
+            self.area >= MIN_AREA):
+            return True
+        return False
+
+    #returns the distance from the buoy in meters.
+    def calculate_distance(self):
+        buoyHeightOnSensor = self.h / PIXELS_PER_MM
+        return ((OBJECT_HEIGHT * (FOCAL_LENGTH / buoyHeightOnSensor)) / 1000.0)
+
+    def calculate_bearing(self, img):
+        # midpoint = len(img[0]) / 2.0 # the midpoint pixel along the x axis.
+        # pixelsOffCenter = (self.x + (self.w / 2.0)) - midpoint
+        # distanceFromCenter = (pixelsOffCenter * OBJECT_HEIGHT) / self.h  # solve the proportion to find the distance in mm
+        
+        # #TODO change method of calculating bearing. this method is off.
+        # bearing = math.degrees(math.asin(distanceFromCenter / self.distance))
+
+        return 0.0
 
 
-method = int(raw_input("1 for camera 2 for saved image: "))
+    def __str__(self):
+        return "This candidate buoy is " + str(self.distance) + " meters away at a bearing of " + str(self.bearing) + "."
 
-img = None
-if (method == 1):
-    cam = cv2.VideoCapture(0)
-    temp = time.time()
-    while ((time.time() - temp) < 1):
-        img = cam.read()[1]
 
-elif (method == 2):
-    img = cv2.imread(raw_input("name of the file in this working directory: "))
+def filter_for_color(img):
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# Convert BGR to HSV
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # define range of the color of the buoy (Todo find the right HSV values)
+    lower_range = np.array([0,100,100])
+    upper_range = np.array([40,255,255])
 
-# define range of the color of the buoy (Todo find the right HSV values)
-lower_range = np.array([0,100,100])
-upper_range = np.array([40,255,255])
+    # Threshold the HSV image to get only the buoy colors
+    return cv2.inRange(hsv, lower_range, upper_range)
 
-# Threshold the HSV image to get only the buoy colors
-mask = cv2.inRange(hsv, lower_range, upper_range)
 
-# #draw the contours around the object.
-contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(mask, contours, -1, (255,0,0), 1)
+def detect_buoys(contours, mask):
+    contourList = []
 
-for i in range(2):
-    maxBoundingRectArea = 0.0
-    maxRect = None
-    maxRectIndex = 0
-    x,y,w,h = (0.0, 0.0, 0.0, 0.0)
+    for j in range(len(contours)):
+        cont = Contour(contours[j])
+        if not cont.is_potential_buoy():
+            continue
 
-    if contours != []:
-        for j in range(len(contours)):
-            if not is_potential_buoy(contours[j]):
-                continue
+        cont.distance = cont.calculate_distance()
+        cont.bearing = cont.calculate_bearing(mask)
+        contourList.append(cont)
+        
+    return contourList
 
-            x,y,w,h = cv2.boundingRect(contours[j])
-            if (w * h > maxBoundingRectArea):
-                maxRect = contours[j]
-                maxRectIndex = j
-                maxBoundingRectArea = w*h
 
-        if not maxRect is None:
-            x, y, w, h = cv2.boundingRect(maxRect)
-            cv2.rectangle(mask, (x, y), (x+w, y+h), (255,255,255), 2)
-            
-            buoyHeightOnSensor = h / PIXELS_PER_MM
-            distanceFromBouy = OBJECT_HEIGHT * (FOCAL_LENGTH / buoyHeightOnSensor) #distance we are away from the buoy in meters
+if __name__ == "__main__":
+    #code for demo ... Eventually to be deleted.
+    # ------------------------------------------------------------
+    method = int(raw_input("1 for camera 2 for saved image: "))
 
-            midpoint = len(img[0]) / 2.0 # the midpoint pixel along the x axis.
-            pixelsOffCenter = (x + (w / 2.0)) - midpoint
-            
-            distanceFromCenter = (pixelsOffCenter * OBJECT_HEIGHT) / h  # solve the proportion to find the distance in mm
-            
-            #TODO change method of calculating bearing. this method is off.
-            bearing = math.degrees(math.asin(distanceFromCenter / distanceFromBouy))
+    img = None
+    if (method == 1):
+        cam = cv2.VideoCapture(0)
+        temp = time.time()
+        while ((time.time() - temp) < 1):
+            img = cam.read()[1]
 
-            bearing %= 360
+    elif (method == 2):
+        img = cv2.imread(raw_input("name of the file in this working directory: "))
+    # ------------------------------------------------------------
 
-            print("Bouy #" + str(i + 1) + " you are " + str(distanceFromBouy / 1000.0) + " meters away from the buoy.")
-            print("Bouy #" + str(i + 1) + " you are at a bearing of " + str(bearing) + " degrees from the buoy.")
-            
-            is_potential_buoy(maxRect)
-            del contours[maxRectIndex]
+    mask = filter_for_color(img)
 
-while True:
-    cv2.imshow('mask', mask)
-    cv2.imshow('img', img)
+    #extract the contours from the masked image.
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+    #draw the contours onto the masked image.
+    cv2.drawContours(mask, contours, -1, (255,0,0), 1)
+
+    candidate_buoys = detect_buoys(contours, mask)
+
+    for c in candidate_buoys:
+        cv2.rectangle(mask, (c.x, c.y), (c.x + c.w, c.y + c.h), (255,255,255), 2)
+        print c
+
+    while True:
+        cv2.imshow('mask', mask)
+        cv2.imshow('img', img)
