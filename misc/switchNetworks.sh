093358514351fc6cca6bd1dd8a359917d42e0af6
Thomas <edison@ubilinux.(none)>
2016-01-31 16:23:29 +0000

    start of newtork scipt

---
+++ misc/switchNetworks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#switchNetworks.sh      Eric Anderson
+#Switches the wpa config file to prioritize yale or ratchet router
+
+
+echo "Welcome to switch networks.  This command must be run as super user"
+echo "Would you like to switch to Ratchet Router (R) or Yale Wireless (Y)?"
+
