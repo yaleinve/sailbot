4944c6c7c46a23cdbf1fcec282dd5720355b7574
Eric Anderson <eric.t.anderson@yale.edu>
2016-02-13 16:01:53 -0500

    basic tmux launcher

---
+++ launch/RatchetLaunch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#TODO: make sure roscore boots first, change perms so must be sudoed (?), make it pretty
+
+tmux new-session -d -s RatchetStack
+tmux send-keys 'roscore &'
+tmux send-keys 'rosrun captain captain_runner.py' 'C-m'
+tmux split-window -v 
+tmux send-keys 'rosrun tactics tactics_runner.py' 'C-m'
+tmux split-window -v
+tmux send-keys 'rosrun airmar airmar_runner.py' 'C-m'
+tmux split-window -v
+tmux send-keys 'rosrun servo_control servoControl.py' 'C-m'
+tmux split-window -v
+tmux send-keys 'rosrun sails_rudder sails_rudder_runner.py' 'C-m'
+tmux split-window -v 
+tmux -2 attach-session -t RatchetStack
