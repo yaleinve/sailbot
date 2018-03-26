ac3f4f9c3a632c2318499128460a871ecfefed64
Lincoln Berkley <linc.berkley@yale.edu>
2018-03-26 19:56:57 -0400

    Add incomplete simulator package

---
+++ src/simulator/src/sim_runner.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+# sim_runner.py   Linc Berkley  Dec 17
+# Implements a physics-based simulator to test the other nodes
+# Adapated from SailsRudder simulator (written in Go) at https://github.com/acshi/acshi/tree/master/sailing
+
+import rospy
+import math
+
+from lib import gpsCalc
+from sails_rudder.msg import SailsRudderPos
+from airmar.msg import AirmarData
+
+
+class Simulator:
+    def __init__(self):
+        # Coordinate convention: x-axis is NORTH, y-axis is EAST. This makes clockwise rotations positive, with
+        # reference angle north, as used elsewher in this project.
+
+        self.dt = 0.01  # Physics timestep
+        self.feedback_interval = 0.1  # Airmar feedback publication interval
+
+        self.pub = None  # Publisher will be created in self.simulate
+
+        self.wind = Vector.from_polar(10, 0)
+        self.sail_adjust = 5  # Degrees to add to sail positions to account for slack
+
+        # Boat properties in SI units
+        self.pos = Vector.null()
+        self.ang = Vector.null()  # Angular position
+        self.v = Vector.null()
+        self.w = Vector.null()  # Angular velocity
+
+        # Positions requested by sails_rudder
+        self.set_main = 0.0
+        self.set_jib = 0.0
+        self.set_rudder = 0.0
+
+        # Actual sail positions (positive is clockwise when seen from above)
+        self.main = 0.0
+        self.jib = 0.0
+
+    def simulate(self):
+        rospy.init_node('simulator')
+        rospy.Subscriber('/sails_rudder_pos', SailsRudderPos, self.sails_rudder_callback)
+        self.pub = rospy.Publisher('/airmar_data', AirmarData, queue_size=10)
+
+        sim_rate = rospy.Rate(1 / self.dt)  # Hz
+        step_num = 0
+        while not rospy.is_shutdown():
+            self.physics_step()
+            if step_num % int(self.feedback_interval / self.dt) == 0:
+                self.publish_feedback()
+            sim_rate.sleep()
+
+    def sails_rudder_callback(self, msg):
+        # TODO: Add something to account for time to move servos
+        self.set_main = msg.mainPos + self.sail_adjust
+        self.set_jib = msg.jibPos + self.sail_adjust
+        self.set_rudder = msg.rudderPos + self.sail_adjust
+
+    def physics_step(self):
+        # Numerical integration procedure based on https://en.wikipedia.org/wiki/Verlet_integration#Velocity_Verlet
+        self.update_sails()
+        accel, alpha = self.calc_accelerations()
+        self.v = self.v.add(accel.mult(self.dt / 2))
+        self.w = self.w.add(alpha.mult(self.dt / 2))
+
+        self.pos = self.pos.add(self.v.mult(self.dt))
+        self.ang = self.ang.add(self.w.mult(self.dt))
+        self.ang.normalize_angles()
+
+        self.update_sails()
+        accel_2, alpha_2 = self.calc_accelerations()
+        self.v = self.v.add(accel_2.mult(self.dt / 2))
+        self.w = self.w.add(alpha_2.mult(self.dt / 2))
+
+    def publish_feedback(self):
+        msg = AirmarData()
+        msg.heading = self.ang.z
+        msg.amrRoll = self.ang.y  # This will always be 0, so TODO: Implement roll properly -- not essential, but would be nice
+        msg.apWndSpd, msg.apWndDir = self.ap_wind().to_polar()
+        msg.sog, msg.cog = self.v.to_polar()
+        msg.truWndSpd, msg.truWndDir = self.wind.to_polar()
+        # Reasonably hoping that simulation distances never become large enough for spherical geometry to create serious problems
+        msg.lat, msg.long = gpsCalc.gpsVectorOffset(0.0, 0.0, self.pos.xy_angle(), self.pos.mag())
+        self.pub.publish(msg)
+
+    def update_sails(self):
+        max_sail_pos = 90 + self.sail_adjust
+
+        # Wind direction, expressed in sail position conventions (bow->rudder is 0; positive is clockwise when viewed
+        # from above; -180 to 180). This is the "equilibrium position" the wind is trying to push the sails to.
+        wind_rel_dir = angle_180_range(self.ap_wind().xy_angle() - self.ang.xy_angle())
+        # Is the equilibrium position within the sail bounds?
+        wind_pos_possible = max_sail_pos >= wind_rel_dir >= -max_sail_pos
+        # Reperesents directions sails will be pushed by wind -- if positive, sail will be pushed in postive direction
+        main_off_wind = angle_180_range(self.set_main - wind_rel_dir)
+        jib_off_wind = angle_180_range(self.set_jib - wind_rel_dir)
+
+        if main_off_wind * (wind_rel_dir - self.set_main) > 0 and wind_pos_possible:
+            # If wind is pushing sail toward the equilibrium position, and that position is possible, the sail will swing to it
+            self.main = wind_rel_dir
+        else:
+            # Otherwise, it will swing toward the max position that the wind is pushing it toward
+            self.main = math.copysign(max_sail_pos, main_off_wind)
+        if jib_off_wind * (wind_rel_dir - self.set_jib) > 0 and wind_pos_possible:
+            # If wind is pushing sail toward the equilibrium position, and that position is possible, the sail will swing to it
+            self.jib = wind_rel_dir
+        else:
+            # Otherwise, it will swing toward the max position that the wind is pushing it toward
+            self.jib = math.copysign(max_sail_pos, jib_off_wind)
+
+    # Returns (linear acceleration), (angular acceleration)
+    def calc_accelerations(self):
+        # Numbers here are from original Go code -- they don't entirely make sense, but magnitudes aren't super important
+        # to this simulation, and the drag/friction constants would be hard to calculate.
+        # TODO: Might try to recalculate constants
+
+        main_mast = Vector(0, -1, 9)
+        main_rel_center = Vector(0, 5, 0)  # Relative to mast
+        # Add 180 to sail position to adjust to angle convention
+        main_center = Simulator.calc_part_pos(main_mast, main_rel_center, self.ang.z, 180 + self.main)
+
+        jib_mast = Vector(0, -7, 5)  # Jib doesn't actually have a mast, but it acts like it has one here
+        jib_rel_center = Vector(0, 5, 0)
+        jib_center = Simulator.calc_part_pos(jib_mast, jib_rel_center, self.ang.z, 180 + self.jib)
+
+        rudder_base = Vector(0, 6.5, 0)
+        rudder_rel_center = Vector(0, 1, 0)
+        rudder_center = Simulator.calc_part_pos(rudder_base, rudder_rel_center, self.ang.z, 180 + self.set_rudder)
+
+        keel_center = Vector(0, 0, -5)
+        base_mass_center = Vector(0, 0, -1)
+        # Component-multiplied by net torque to get angular acceleration, a 3x3 tensor would be more correct, but this
+        # is easier and probably reasonably accurate
+        inverse_moment_inertia = Vector(0, 0.01, 0.1)
+
+        # These constants (unitless) encapsulate lift/drag/density coefficients with air, water, surface areas, etc...
+        main_constant = 0.05
+        jib_constant = 0.03
+        keel_constant = 40.0  # We also give the keel credit for other lateral drag and friction
+        rudder_constant = 10.0
+
+        # Friction constants (dimensionless)
+        axial_friction = 0.05
+        angular_friction = 3.0
+        forward_friction = 0.5
+
+        grav_force = Vector(0, 0, 50)
+
+
+
+    # Get apparent wind vector (not a field, because it would be a pain to relcalculate it whenever velocity is updated)
+    def ap_wind(self):
+        return self.wind.add(self.v)  # Add, rather than subtract, because "wind direction" is the direction wind is coming from
+
+    # Calculates position of part in global referenc frame when both base and part are rotated
+    @staticmethod
+    def calc_part_pos(base_vec, part_vec, boat_heading, part_heading):
+        part_rotated = part_vec.xy_rotate(part_heading)  # Rotated to reference frame of boat
+        total_unrotated = base_vec.add(part_rotated)  # Total in reference frame of boat
+        return total_unrotated.xy_rotate(boat_heading)
+
+
+# All angle arguments and returns in degrees
+class Vector:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    @classmethod
+    def null(cls):
+        return cls(0.0, 0.0, 0.0)
+
+    @classmethod
+    def from_spherical(cls, rho, theta, phi):
+        theta_r = math.radians(theta)
+        phi_r = math.radians(phi)
+        return cls(rho * math.sin(phi_r) * math.cos(theta_r),
+                   rho * math.sin(phi_r) * math.sin(theta_r),
+                   rho * math.cos(phi_r))
+
+    @classmethod
+    def from_polar(cls, r, theta):
+        return cls.from_spherical(r, theta, 90)
+
+    def mag(self):
+        return math.sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2))
+
+    def xy_angle(self):
+        return math.degrees(math.atan(self.y / self.x))
+
+    def to_polar(self):
+        return self.mag(), self.xy_angle()
+
+    # Should only be called on vectors that represent angles in degrees
+    def normalize_angles(self):
+        self.x, self.y, self.z = [a % 360 for a in (self.x, self.y, self.z)]
+
+    def add(self, v):
+        return Vector(self.x + v.x, self.y + v.y, self.z + v.z)
+
+    def sub(self, v):
+        return Vector(self.x - v.x, self.y - v.y, self.z - v.z)
+
+    def mult(self, a):
+        return Vector(self.x * a, self.y * a, self.z * a)
+
+    def dot(self, v):
+        return self.x * v.x + self.y * v.y + self.z * v.z
+
+    def cross(self, v):
+        return Vector(self.y * v.z - self.z * v.y, self.z * v.x - self.x * v.z, self.x * v.y - self.y * v.x)
+
+    def comp_mult(self, v):
+        return Vector(self.x * v.x, self.y * v.y, self.z * v.z)
+
+    def scalar_proj(self, v):
+        return self.dot(v) / self.mag()
+
+    def vector_proj(self, v):
+        return self.mult(self.dot(v)).mult(1 / pow(self.mag(), 2))
+
+    def xy_rotate(self, a):
+        new = Vector(0, 0, self.z)
+        new.x, new.y = rotate(self.x, self.y, a)
+        return new
+
+
+# Return angle in -180 to 180 range that is equivalent to input angle
+def angle_180_range(a):
+    return ((a + 180) % 360) - 180
+
+
+# Rotates in standard direction (from x-axis to y-axis, clockwise under our conventions)
+def rotate(x, y, a):
+    ar = math.radians(a)
+    return (x * math.cos(ar) - y * math.sin(ar)), (x * math.sin(ar) + y * math.cos(ar))
+
+
+if __name__ == '__main__':
+    sim = Simulator()
+    sim.simulate()
