83e3b3170c7ff221d3f4b871246fbbeee1a42045
Lincoln Berkley <linc.berkley@yale.edu>
2018-11-02 13:32:51 -0400

    Fix tmux-based simulator launch file

--- launch/SimLaunch.sh
+++ launch/SimLaunch.sh
@@ -4,9 +4,6 @@ tmux new-session -d -s SimStack
 tmux split-window -v
 tmux split-window -v
 tmux split-window -v
-tmux split-window -v
-tmux split-window -v
-tmux split-window -v
 tmux select-layout tile
 
 tmux select-pane -t 0
@@ -21,13 +18,19 @@ tmux send-keys 'rosrun captain captain_runner.py' 'C-m'
 tmux select-pane -t 3
 tmux send-keys 'rosrun tactics tactics_runner.py' 'C-m'
 
-tmux select-pane -t 4
+tmux new-window -a
+tmux split-window -v
+tmux split-window -v
+tmux split-window -v
+tmux select-layout tile
+
+tmux select-pane -t 0
 tmux send-keys 'rosrun sails_rudder sails_rudder_runner.py' 'C-m'
 
-tmux select-pane -t 5
+tmux select-pane -t 1
 tmux send-keys 'rosrun speed_calculator speed_calculator_runner.py' 'C-m'
 
-tmux select-pane -t 6
+tmux select-pane -t 2
 tmux send-keys 'rosrun simulator sim_runner.py' 'C-m'
 
 tmux -2 attach-session -t SimStack
