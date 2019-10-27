7e6cdbb566fdf5ec0fbc505e9e8db38396c30799
Thomas <edison@ubilinux.local>
2019-10-27 14:37:40 -0400

    Get XBee receive script to run

--- scripts/xbee_read_write.py
+++ scripts/xbee_read_write.py
@@ -4,13 +4,18 @@ import time
 import mraa
 
 def main():
-    s = mraa.Spi()
+    s = mraa.Spi(0)
+    s.frequency(1000000)
     while True:
-        header = s.write(bytearray([0]*4))
-        if header[0] == 0x7E:  # Start delimiter, see XBee User Guide for full format
-            length = header[1] << 8 + header[2]
-            msg_type = header[3]
-            contents = s.write(bytearray([0]*(length-2)))
+        header = s.write(bytearray([0]*5))
+        if header[1] == 0x7E:  # Start delimiter, see XBee User Guide for full format.
+            # Header begins at second byte after XBee recieves message, highly likely
+            # to be the second byte in a 5-byte sequence.
+	    print "Header: " + repr(header)
+            length = (header[2] << 8) + header[3]
+            print "Length: " + str(length)
+            msg_type = header[4]
+            contents = s.write(bytearray([0]*(length-1)))
             checksum = s.write(bytearray([0]))
             if msg_type == 0x90:
                 print "Received: " + contents[11:]
@@ -19,7 +24,7 @@ def main():
                 print "Not a recieve: " + repr(contents)
         else:
             print "No message: " + repr(header)
-        time.sleep(500);
+        time.sleep(0.5);
 
 if __name__ == "__main__":
     main()
