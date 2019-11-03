5df7776acdd228baddcda40fa5250928e3836d73
Lincoln Berkley <lincoln.berkley@gmail.com>
2019-11-03 13:38:08 -0500

    Prepare to implment write

--- scripts/xbee_read_write.py
+++ scripts/xbee_read_write.py
@@ -1,30 +1,54 @@
 #!/usr/bin/python
 
+import sys
 import time
 import mraa
 
+POLL_PERIOD = 0.5
+DEBUG = False
+
 def main():
     s = mraa.Spi(0)
+    # Setting the freqency is necessary to get meaningful values, but it's unclear what the
+    # default frequency is and why it doesn't work.
     s.frequency(1000000)
     while True:
-        header = s.write(bytearray([0]*5))
-        if header[1] == 0x7E:  # Start delimiter, see XBee User Guide for full format.
-            # Header begins at second byte after XBee recieves message, highly likely
-            # to be the second byte in a 5-byte sequence.
-	    print "Header: " + repr(header)
-            length = (header[2] << 8) + header[3]
-            print "Length: " + str(length)
-            msg_type = header[4]
-            contents = s.write(bytearray([0]*(length-1)))
-            checksum = s.write(bytearray([0]))
-            if msg_type == 0x90:
-                print "Received: " + contents[11:]
-                print "Full contents: " + repr(contents)
-            else:
-                print "Not a recieve: " + repr(contents)
-        else:
-            print "No message: " + repr(header)
-        time.sleep(0.5);
+        finish_read(s, bytearray())
+        time.sleep(POLL_PERIOD);
+
+def finish_read(spi, buf):
+    start_loc = buf.find(0x7E)  # Start delimiter, see XBee User Guide for full format.
+    if start_loc != -1:
+        buf = buf[start_loc:]
+    else:
+        buf = bytearray()
+
+    while True:
+        start, buf = buffered_read(spi, buf, 1)[0]
+        if start != 0x7E:
+            break
+
+        header, buf = buffered_read(spi, buf, 3)
+        length = (header[0] << 8) + header[1]
+        assert header[2] == 0x90, "Message type not receive: %02x" % header[2]
+
+        contents, buf = buffered_read(spi, buf, length - 1)
+        checksum, buf = buffered_read(spi, buf, 1)  # TODO: verify checksum
+
+        print "Received: %s" % contents[11:]  # Message text starts at 11th byte
+
+def buffered_read(spi, buf, count):
+    if count <= len(buf):
+        return buf[:count], buf[count:]
+    elif len(buf) > 0:
+        new_bytes = spi.write(bytearray([0] * (count - len(buf))))
+        return buf + new_bytes, bytearray()
+    else:
+        return spi.write(bytearray([0] * count)), bytearray()
+
+def debug_print(string):
+    if DEBUG:
+        print string
 
 if __name__ == "__main__":
     main()
